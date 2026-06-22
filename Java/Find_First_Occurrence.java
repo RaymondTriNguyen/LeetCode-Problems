3a07b62e3480dbcd51022adc3e37523ecf6a0185
Raymond Nguyen <137119948+RaymondTriNguyen@users.noreply.github.com>
2026-06-22 13:14:52 -0500

    Add strStr method to find first occurrence in string
    Implement strStr method to find first occurrence of a substring.

---
+++ Java/Find_First_Occurrence.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+       /* for(int i = 0; i < haystack.length(); i++){
+            for(int j = 0; j < needle.length(); j++){
+
+            }
+        } */
+
+        return haystack.indexOf(needle);
+
+    }
+}
