9612001b7e2bbadb772e526f22472af70f027e2a
Raymond Nguyen <137119948+RaymondTriNguyen@users.noreply.github.com>
2026-06-22 12:21:40 -0500

    Add removeElement function to remove specified value
    Implement removeElement function to remove specified value from list.

---
+++ Python/Remove_Element.py
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/remove-element/description/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        indexUpdate = 0
+        for i in range(0, len(nums)):
+            if nums[i]!=val:
+                nums[indexUpdate] = nums[i]
+                indexUpdate += 1
+
+        return indexUpdate
